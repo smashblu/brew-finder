e82f7d13e55030e5ba11b1c9b0f1083e03334164
smashblu <smashblu@gmail.com>
2025-01-14 14:50:01 -0800

    Add map to flexbox

---
+++ resources/scripts.js
@@ -0,0 +1,6 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoic21hc2hibHUiLCJhIjoiY201eDF0dTI5MDRpMTJqcTVieTNuZHNweCJ9.ynSYSc_J3rnPLBf9zR3rWw';
+const map = new mapboxgl.Map({
+        container: 'map',
+        center: [-74.5, 40],
+        zoom: 9
+});
