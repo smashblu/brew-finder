4261a714c0eacdbd2d5477da05045eb1934761dc
smashblu <98678476+smashblu@users.noreply.github.com>
2025-03-06 14:42:45 -0800

    Test creation of button component

--- src/app/ui/main.tsx
+++ src/app/ui/main.tsx
@@ -1,13 +1,19 @@
+export function SubmitButton({ text, style }) {
+        return (
+                <button className={style}>{text}</button>
+        )
+}
+
 export function Sidebar() {
         return (
                 <div className="sidebar">
                         <div className="col-lg-4 enter-zip" id="sub-side">
-                                <label for="location-with-zip" className="form-label">Zip Code</label>
+                                <label htmlFor="location-with-zip" className="form-label">Zip Code</label>
                                 <div className="side-input">
                                         <input type="text" className="form-control" id="location-with-zip" placeholder="12345" />
                                 </div>
                                 <div className="side-button">
-                                        <button type="button" id="submit-zip" className="btn btn-primary">Submit</button>
+                                        <SubmitButton text={'Submit'} style={'side-button'} />
                                 </div>
                         </div>
                 </div>
