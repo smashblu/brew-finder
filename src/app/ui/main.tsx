68f332aa4c20c7290e349364453efa328f45ca4b
smashblu <smashblu@gmail.com>
2025-03-10 21:31:13 -0700

    Create form to handle zip input

--- src/app/ui/main.tsx
+++ src/app/ui/main.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import '@/styles/main.css'
 
 export function FormLabel({ text, style, htmlfor }) {
@@ -7,34 +9,40 @@ export function FormLabel({ text, style, htmlfor }) {
 }
 
 export function FormButton({ text, style, handleClick }) {
+        console.log(text, style, handleClick)
         return (
-                <button className={style} onClick={handleClick}>{text}</button>
+                <button className={style}>{text}</button>
         )
 }
 
 export function FormInput({ type, style, id, placeholder }) {
         return (
-                <input type={type} className={style} id={id} placeholder={placeholder}></input>
+                <input name='zip' type={type} className={style} id={id} placeholder={placeholder}></input>
         )
 }
 
 export function Sidebar() {
-        function handleClick(i) {
-                if (i) {
-                        return <h1>{i}</h1>
+        function handleClick(e) {
+                const zipCode = e.get('zip')
+                console.log(`e: ${e}`, `zipcode: ${zipCode}`)
+                if (zipCode) {
+                        console.log(`true: ${zipCode}`)
+                        return <h1>{zipCode}</h1>
                 }
         }
 
         return (
                 <div>
                         <div id="sub-side">
-                                <FormLabel htmlfor={'location-with-zip'} style={'form-label'} text={'Zip Code'} />
-                                <div className="side-input">
-                                        <FormInput type={'text'} style={'form-control'} id={'location-with-zip'} placeholder={'12345'} />
-                                </div>
-                                <div className="side-button">
-                                        <FormButton text={'Submit'} style={'side-button'} onClick={handleClick('test')} />
-                                </div>
+                                <form action={handleClick}>
+                                        <FormLabel htmlfor={'location-with-zip'} style={'form-label'} text={'Zip Code'} />
+                                        <div className="side-input">
+                                                <FormInput type={'text'} style={'form-control'} id={'location-with-zip'} placeholder={'12345'} />
+                                        </div>
+                                        <div className="side-button">
+                                                <FormButton text={'Submit'} style={'side-button'} />
+                                        </div>
+                                </form>
                         </div>
                 </div>
         )
