edb61cb4f22bd14055e785b9e36207c2025f3d26
smashblu <98678476+smashblu@users.noreply.github.com>
2025-03-18 14:23:20 -0700

    Merge buttons.tsx with main

--- src/app/ui/main.tsx
+++ src/app/ui/main.tsx
@@ -1,7 +1,30 @@
 import '@/styles/main.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import mapboxgl from 'mapbox-gl';
-import { FormButton, FormInput, FormLabel } from '@/ui/buttons';
+
+export function FormLabel({ text, style, htmlfor }) {
+  return (
+    <label htmlFor={htmlfor} className={style}>
+      {text}
+    </label>
+  );
+}
+
+export function FormButton({ text, style, handleClick }) {
+  return <button className={style}>{text}</button>;
+}
+
+export function FormInput({ type, style, id, placeholder }) {
+  return (
+    <input
+      name="zip"
+      type={type}
+      className={style}
+      id={id}
+      placeholder={placeholder}
+    ></input>
+  );
+}
 
 export async function Sidebar() {
   async function handleClick(e) {
