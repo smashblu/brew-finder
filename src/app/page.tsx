923d346e320644d6bb575b877ffe1b0626871611
smashblu <smashblu@gmail.com>
2025-03-24 22:35:30 -0700

    Add Marker component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import '@/styles/styles.css';
 import { Header } from '@/ui/header'
 import { DrawMap } from '@/ui/map'
 import { Sidebar } from '@/ui/sidebar'
+import { TestScope } from '@/ui/marker'
 
 export default function Page() {
   return (
