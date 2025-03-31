dcffddde059f3819bd30c8bb9787333e4d875af5
smashblu <98678476+smashblu@users.noreply.github.com>
2025-03-31 13:41:58 -0700

    Fix page.tsx imported components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@
 
 import '@/styles/styles.css';
 import { Header } from '@/ui/header'
-import { Sidebar, DrawMap, TestScope } from '@/ui/main'
+import { Main, DrawMap, TestScope } from '@/ui/main'
 
 export default function Page() {
   return (
     <div className="full=page">
       <Header />
       <div className="main">
-        <Sidebar />
+        <Main />
         <DrawMap />
       </div>
     </div>
